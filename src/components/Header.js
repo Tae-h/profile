c89630470dae3edca924308f5216d997e64d1d78
Tae-h <zppy13@gmail.com>
2022-06-28 23:11:44 +0900

    이미지 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import {memo} from "react";
-import {Layout, Menu} from "antd";
+import {Image, Layout, Menu} from "antd";
 import '../css/Header.css';
 import {IdcardOutlined, ProfileOutlined} from "@ant-design/icons";
+import sproutImage from '../assets/images/icon-sprout.png';
 const {Header} = Layout;
+
 
 
 
@@ -23,7 +25,10 @@ const HeaderComponent = memo(({setPage}) => {
 
     return (
         <Header className={'site-header'}>
-            <span style={{ fontFamily: 'serif', fontSize: ''}}>DEVELOPER</span>
+            <span style={{ fontFamily: 'serif'}}>
+                DEVELOPER
+            </span>
+            <Image src={sproutImage} preview={false} style={{ marginLeft: '10px', height: '50%' }}/>
             <Menu mode="horizontal" theme="light" defaultSelectedKeys={["profile"]} style={{width: '100%', justifyContent: 'end'}}>
                 <Menu.Item key="profile" icon={<IdcardOutlined />} title="profile" onClick={ clickProfile}>
                     PROFILE
